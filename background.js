15c14eae1a1a221d7252c48fc41b0cca064529d7
noordean <ebroyeem90@gmail.com>
2019-12-06 11:58:59 +0100

    Make request to quran API

--- background.js
+++ background.js
@@ -1,5 +1,24 @@
-chrome.runtime.onInstalled.addListener(() => {
-  // Do something in the background
+const fetchVerse = async () => {
+  const response = await fetch(
+    "http://api.alquran.cloud/v1/search/death/all/en.ahmedali",
+    {
+      method: "GET",
+      headers: {
+        Accept: "application/json"
+      }
+    }
+  );
+  const result = await response.json();
+  alert(JSON.stringify(result.data));
+  return result;
+};
 
-  //   alert("Reminder!!!");
+chrome.runtime.onInstalled.addListener(() => {
+  chrome.alarms.create("fetch_verses", { periodInMinutes: 1 });
+});
+
+chrome.alarms.onAlarm.addListener(alarm => {
+  if (alarm.name === "fetch_verses") {
+    fetchVerse();
+  }
 });
